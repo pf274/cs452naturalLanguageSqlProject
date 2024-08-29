b4bbea773e3bb1b7a9eab6fb8e5cd0d3bb23075a
pf274 <peterfullmer33@gmail.com>
2024-08-28 18:32:30 -0600

    Got it working on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["src/**/*.wasm"],
-  base: "cs452naturallanguagesqlproject",
+  base: "cs452naturalLanguageSqlProject",
 });
