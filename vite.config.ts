096a64b2bbb2eefd3e1ee160c65e84dc204f4591
pf274 <peterfullmer33@gmail.com>
2024-08-29 14:43:24 -0600

    Included sqlite as asset

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["src/**/*.wasm"],
+  assetsInclude: ["src/**/*.wasm", "src/**/*.sqlite"],
   base: "/cs452naturalLanguageSqlProject",
 });
